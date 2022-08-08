38c37a88e8adda7f7d2096caf914bb970123b617
thanhnha <nha.tt@vn-cubesystem.com>
2022-08-08 13:26:01 +0700

    feat: initial code

--- App.js
+++ App.js
@@ -1,19 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Pedometer } from 'expo-sensors';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [isPedometerAvailable, setIsPedometerAvailable] = useState('checking');
+  const [pastStepCount, setPastStepCount] = useState(0);
+  const [currentStepCount, setCurrentStepCount] = useState(0);
+
+  let _subscription;
+
+  const _subscribe = () => {
+    _subscription = Pedometer.watchStepCount(result => {
+      setCurrentStepCount(result.steps);
+    });
+
+    Pedometer.isAvailableAsync().then(
+      result => {
+        setIsPedometerAvailable(result);
+      },
+      error => {
+        setIsPedometerAvailable('Could not get isPedometerAvailable: ' + error);
+      }
+    );
+
+    const end = new Date();
+    const start = new Date();
+    start.setDate(end.getDate() - 1);
+    Pedometer.getStepCountAsync(start, end).then(
+      result => {
+        setPastStepCount(result.steps);
+      },
+      error => {
+        setPastStepCount('Could not get stepCount: ' + error);
+      }
+    );
+  };
+
+  const _unsubscribe = () => {
+    _subscription && _subscription.remove();
+    _subscription = null;
+  };
+
+
+  useEffect(()=>{
+    _subscribe();
+    return ()=> _unsubscribe();
+  },[])
+
+    return (
+      <View style={styles.container}>
+        <Text>Pedometer.isAvailableAsync(): {isPedometerAvailable}</Text>
+        <Text>Steps taken in the last 24 hours: {pastStepCount}</Text>
+        <Text>Walk! And watch this go up: {currentStepCount}</Text>
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginTop: 15,
     alignItems: 'center',
     justifyContent: 'center',
   },
